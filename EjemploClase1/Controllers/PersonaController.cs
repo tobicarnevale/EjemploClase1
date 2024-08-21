e0c4f6672c5d44b589b651385840c0aec7aa70dd
tobicarnevale <tobicarnevale@gmail.com>
2024-08-21 18:23:14 -0300

    Agregar archivos de proyecto.

---
+++ EjemploClase1/Controllers/PersonaController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EjemploClase1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaController : ControllerBase
+    {
+
+
+        // POST api/<Persona>
+        [HttpPost]
+        public List<Persona> GetPersonas(Persona persona)
+        {
+            List<Persona> lista = new List<Persona>();
+            for (int i = 0; i < 10; i++)
+            {
+                lista.Add(persona);
+            }
+                return lista;
+        }
+
+
+
+    
+    }
+}
---
+++ EjemploClase1/Persona.cs
@@ -0,0 +1,10 @@
+﻿namespace EjemploClase1
+{
+    public class Persona
+    {
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public int edad {  get; set; }
+        public string profesion {  get; set; }
+    }
+}
